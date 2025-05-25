e9835729a6b4221aabc0e54577baf02bdab5e45c
Flor <florencia.ortiz096@gmail.com>
2025-05-25 13:30:06 -0300

    configuración del proyecto y dependencias

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { PORT } from './config.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>hello wordl</h1>')
+})
+
+app.post('/login', (req, res) => {
+  res.json({ user: 'holis' })
+})
+app.post('/register', (req, res) => { })
+app.post('/logout', (req, res) => { })
+
+app.post('/protected', (req, res) => { })
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+})
